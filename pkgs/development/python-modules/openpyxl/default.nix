9612bb6dd5c0b013d221467b51f86532bde329fb
Sam Hug <s@m-h.ug>
2019-03-22 13:32:55 -0700

    python: openpyxl: 2.6.0 -> 2.6.1

--- pkgs/development/python-modules/openpyxl/default.nix
+++ pkgs/development/python-modules/openpyxl/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "openpyxl";
-  version = "2.6.0";
+  version = "2.6.1";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "70da6b45a5925285b6a3d93570b45f4402eb2d335740163a58eef533b139565c";
+    sha256 = "9e4db4ee7aadd0ff7a814f7483b2d94e6b8d4f14dff780b023c5bdc94af54dd5";
   };
 
   checkInputs = [ pytest ];
